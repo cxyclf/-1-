c8cfb359d24752679948b12f3b600e22ba11233f
cxyclf <67739604+cxyclf@users.noreply.github.com>
2021-06-30 23:41:13 +0800

    Add files via upload

--- src/cn/edu/whut/sept/zuul/Command.java
+++ src/cn/edu/whut/sept/zuul/Command.java
@@ -1,33 +1,39 @@
 package cn.edu.whut.sept.zuul;
 
-public class Command
-{
+public class Command {
+    /**第一个命令字段.*/
     private String commandWord;
+    /**第二个命令字段.*/
     private String secondWord;
 
-    public Command(String firstWord, String secondWord)
-    {
+    /**将键盘输入的命令赋予给字段.
+     * @param firstWord 键盘输入的第一个命令
+     *@param secondWord 键盘输入的第二个命令*/
+    public Command(String firstWord, String secondWord) {
         commandWord = firstWord;
         this.secondWord = secondWord;
     }
 
-    public String getCommandWord()
-    {
+    /**@return 用return方法暴露commandWord.*/
+    public String getCommandWord() {
         return commandWord;
     }
 
-    public String getSecondWord()
-    {
+    /**@return 用return方法暴露secondWord.*/
+    public String getSecondWord() {
         return secondWord;
     }
 
-    public boolean isUnknown()
-    {
+    /**用一个布尔型判断commandWord是否为空.
+     * @return 返回commandWord==null的结果，若相等返回ture，不相等返回false.*/
+    public boolean isUnknown() {
         return (commandWord == null);
     }
 
-    public boolean hasSecondWord()
-    {
+    /**用一个布尔型判断commandWord是否为空.
+     * @return 返回commandWord==null的结果，若相等返回ture，不相等返回false*/
+    public boolean hasSecondWord() {
         return (secondWord != null);
     }
+
 }
